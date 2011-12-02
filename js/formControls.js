e0af4dfd0ea144766e656ba9d2c1f66ce68ec250
Foxfire <foxfire@evilcupcakes.net>
2011-12-02 16:28:04 -0600

    * CReated form controls javascript * made modification to application html.

---
+++ js/formControls.js
@@ -0,0 +1,33 @@
+function clearForm() {
+    var elements = document.getElementById('theForm').elements;
+
+    for(var i = 0; i < elements.length; ++i) {
+        if (elements[i].type == "checkbox") {
+            elements[i].checked = false;
+        } else if (elements[i].type != "submit" && elements[i].type != "reset") {
+            elements[i].value = ""
+        } else if (elements[i].type == "select") {
+            elements[i].selectedIndex = 0;
+        }
+    }
+}
+
+function alreadyHaveInfo() {
+    var alreadyHave = 'Please use 2011\'s info.';
+    var competitorBox = document.getElementsByName('returningCompetitor')[0];
+    var elements = document.getElementById('theForm').elements;
+
+    if (!competitorBox.checked) {
+        alreadyHave = "";
+    }
+
+    for (var i = 0; i < elements.length; ++i) {
+        var name = elements[i].name;
+
+        if(name == 'address' || name == 'city' || name== 'state' || name == 'zip' || name== 'country'
+            || name == 'phone' || name == 'email' || name == 'bio' || name =='height' || name == 'weight'
+            || name == 'joustingSince' || name == 'occupation' || name == 'motto') {
+            elements[i].value = alreadyHave;
+        }
+    }
+}
