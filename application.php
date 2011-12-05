d86607a27b8aeaeedbb3cfc9a361ddb99f5a74c5
Foxfire <foxfire@evilcupcakes.net>
2011-12-05 01:55:17 -0600

    Formatting for A'Plaisance website.

---
+++ application.php
@@ -0,0 +1,31 @@
+<html>
+
+    <head>
+        <title>Lysts 2012 Competitor application</title>
+        <script type="text/javascript" src="http://closure-library.googlecode.com/svn/trunk/closure/goog/base.js"></script>
+        <script type="text/javascript">
+            goog.require('goog.dom');
+            goog.require('goog.json');
+            goog.require('goog.proto2.ObjectSerializer');
+            goog.require('goog.string.StringBuffer');
+        </script>
+        <script type="text/javascript" src="js/libphonenumber/i18n/phonenumbers/phonemetadata.pb.js"></script>
+        <script type="text/javascript" src="js/libphonenumber/i18n/phonenumbers/phonenumber.pb.js"></script>
+        <script type="text/javascript" src="js/libphonenumber/i18n/phonenumbers/metadata.js"></script>
+        <script type="text/javascript" src="js/libphonenumber/i18n/phonenumbers/phonenumberutil.js"></script>
+        <script type="text/javascript" src="js/libphonenumber/i18n/phonenumbers/asyoutypeformatter.js"></script>
+
+        <script type="text/javascript" src="js/formValidation.js"></script>
+     <script type="text/javascript" src="js/formControls.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+    </head>
+    <body style="background-image: url('img/parchment.jpg');">
+        <div id="contentPane" style="width:900px; margin: 0 auto; background-color: #ffffff;">
+            <img src="img/apLogo.jpg"/>
+            <img src="img/woodBeam.png" style="position: relative; top:0px; left:0px;"/>
+
+            <img src="img/application.png" style="margin: 0 auto;"/>
+            <?php include('form.html'); ?>
+        </div>
+    </body>
+</html>
