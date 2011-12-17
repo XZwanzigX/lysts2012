e149fa02e2829b9b34ce35282cbeb0af01fbccac
Foxfire <foxfire@evilcupcakes.net>
2011-12-17 01:45:04 -0600

    Added some styles to the table, provided cleaner defaults.

--- review/display.php
+++ review/display.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>Lysts Application Review</title>
+        <link rel="stylesheet" type="text/css" href="../css/appReview.css"/>
     </head>
 
     <body>
@@ -20,34 +21,34 @@ function passwordsCorrect() {
 function writeTableHeader() {
     echo "<table class='appTable'>\n";
     echo "\t<tr>\n";
-    echo "\t\t<td class=\"tableHeader\">First Name</td>";
-    echo "\t\t<td class=\"tableHeader\">Last Name</td>";
-    echo "\t\t<td class=\"tableHeader\">Address</td>";
-    echo "\t\t<td class=\"tableHeader\">City</td>";
-    echo "\t\t<td class=\"tableHeader\">State</td>";
-    echo "\t\t<td class=\"tableHeader\">Zip</td>";
-    echo "\t\t<td class=\"tableHeader\">Country</td>";
-    echo "\t\t<td class=\"tableHeader\">Phone</td>";
-    echo "\t\t<td class=\"tableHeader\">Email</td>";
-    echo "\t\t<td class=\"tableHeader\">Skill At Arms</td>";
-    echo "\t\t<td class=\"tableHeader\">Melee a Cheval</td>";
-    echo "\t\t<td class=\"tableHeader\">Joust</td>";
-    echo "\t\t<td class=\"tableHeader\">Experience</td>";
-    echo "\t\t<td class=\"tableHeader\">IJL Member?</td>";
-    echo "\t\t<td class=\"tableHeader\">Hauling Horses?</td>";
-    echo "\t\t<td class=\"tableHeader\">Stalls Needed</td>";
-    echo "\t\t<td class=\"tableHeader\">Bio</td>";
-    echo "\t\t<td class=\"tableHeader\">Theme Song</td>";
-    echo "\t\t<td class=\"tableHeader\">Artist</td>";
-    echo "\t\t<td class=\"tableHeader\">Start</td>";
-    echo "\t\t<td class=\"tableHeader\">End</td>";
-    echo "\t\t<td class=\"tableHeader\">Height</td>";
-    echo "\t\t<td class=\"tableHeader\">Weight</td>";
-    echo "\t\t<td class=\"tableHeader\">Started Jousting</td>";
-    echo "\t\t<td class=\"tableHeader\">Occupation</td>";
-    echo "\t\t<td class=\"tableHeader\">Motto</td>";
-    echo "\t\t<td class=\"tableHeader\">Favorite Drink</td>";
-    echo "\t\t<td class=\"tableHeader\">Submission Date</td>";
+    echo "\t\t<td class=\"tableHeader\">First Name</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Last Name</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Address</td>\n";
+    echo "\t\t<td class=\"tableHeader\">City</td>\n";
+    echo "\t\t<td class=\"tableHeader\">State</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Zip</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Country</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Phone</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Email</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Skill At Arms</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Melee a Cheval</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Joust</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Experience</td>\n";
+    echo "\t\t<td class=\"tableHeader\">IJL Member?</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Hauling Horses?</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Stalls Needed</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Bio</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Theme Song</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Artist</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Start</td>\n";
+    echo "\t\t<td class=\"tableHeader\">End</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Height</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Weight</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Started Jousting</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Occupation</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Motto</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Favorite Drink</td>\n";
+    echo "\t\t<td class=\"tableHeader\">Submission Date</td>\n";
     echo "\t</tr>\n";
 }
 
@@ -55,8 +56,20 @@ function writeRow($row, $i) {
     $rowClass = $i % 2 ? "regularRow" : "shadedRow";
     echo "\t<tr class=\"" . $rowClass . "\">";
 
-    foreach ($row as $value) {
-        echo "\t\t<td class=\"tableCell\">" . $value . "</td>\n";
+    foreach ($row as $key=>$value) {
+        $val = stripcslashes($value);
+        echo "\t\t<td>";
+        if (strpos('skill_at_arms,melee_a_cheval,joust,ijl_member,hauling_horses', $key) !== false) {
+            echo $value == 1 ? "YES" : "NO";
+        } elseif ('height' == $key) {
+            echo preg_match('/Please use 2011\'s info/', $val) ? $val : str_replace('\\', "\"", $val);
+        } elseif (strpos('started_jousting,weight', $key) !== false) {
+            echo $val == '0' ? "Please use 2011's info." : $val;
+        } else {
+            echo $val == '' ? "&nbsp;" : $val;
+        }
+
+        echo "</td>\n";
     }
 
     echo "\t</tr>\n";
@@ -64,41 +77,17 @@ function writeRow($row, $i) {
 
 function displayApplications() {
     newReadConnection();
-    $sql = "select first_name," .
-            "last_name,
-            address,
-            city,
-            state,
-            zip,
-            country,
-            phone,
-            email,
-            skill_at_arms,
-            melee_a_cheval,
-            joust,
-            experience,
-            ijl_member,
-            hauling_horses,
-            stalls_needed,
-            bio,
-            song,
-            artist,
-            start,
-            end,
-            height,
-            weight,
-            started_jousting,
-            occupation,
-            motto_and_translation,
-            favorite_drink
-            submission_date,
-            from application_2012";
+    $sql = "select first_name, last_name, address, city, state, zip, country, phone, email, skill_at_arms, " .
+           "melee_a_cheval, joust, experience, ijl_member, hauling_horses, stalls_needed, bio, song, artist, " .
+           "start, end, height, weight, started_jousting, occupation, motto_and_translation, favorite_drink, submission_date " .
+           "from application_2012";
     $result = mysql_query($sql);
 
     writeTableHeader();
     $i = 0;
     while ($row = mysql_fetch_assoc($result)) {
         writeRow($row, $i);
+        ++$i;
     }
 }
 
